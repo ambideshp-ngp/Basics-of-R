4e45d884d9c985bfa502449b7d728c0b4f589ab8
ambideshp-ngp <70280049+ambideshp-ngp@users.noreply.github.com>
2020-08-27 19:44:03 +0530

    Add files via upload

---
+++ Practice.R
@@ -0,0 +1,42 @@
+1+1
+2*pi*6378
+2+3*4
+(2+3)*5+3
+2+(3*4)
+x<-1
+y<-2
+z<--3
+x+y*z
+This.year<-2004
+This.year
+35+30
+#Create a vector
+dbl_var<-c(1,2.5,4.5,2,2.3,4.5,12,1)
+x<-c(2,4,5,6,10)
+y<-c(2,7,8,9,10)
+#Adding vectors
+z<-x+y
+#repeating value n times
+rep(1,12)
+#sequencing numbers from one to five
+seq(1,5)
+sqrt(x)
+x*y
+#Accressing vector elements
+x[1]
+x[4]
+x[5]
+#changing any value in vector use assignment operator to change it
+x[2]<-3
+print(x)
+print(x[3])
+#asigning character values.chara values to be either between single and double quotes
+chara<-c('a','b','c',10,100,9)
+print[chara[1]]
+print(chara)
+df<-data.frame(x=1:3,y=c('a','b','c'))
+df<-data.frame(x=1:3,x=1:3)
+df1<-data.frame(x=c(1,2,3),y=c('a','b','c'))
+#slicing the data frame
+df[1,1]
+df[1,2]
